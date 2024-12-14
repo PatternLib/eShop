6085c9da23df849aa88840ce2391a0bfe72f12fd
Robinson Batista <manuel7@outlook.es>
2024-12-14 04:03:32 -0500

    The Main method of Program.cs has been simplified

--- source/Web/WebMvc/Program.cs
+++ source/Web/WebMvc/Program.cs
@@ -1,35 +1,19 @@
+using Microsoft.AspNetCore;
+
 namespace EShopOnContainers.WebMvc;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        BuildWebHost(args)
+            .Run();
+    }
 
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
+    public static IWebHost BuildWebHost(string[] args)
+    {
+        return WebHost.CreateDefaultBuilder(args: args)
+            .UseStartup<Startup>()
+            .Build();
     }
 }
