4fadc2e46da517b5dd4893b90139e6058242e14f
Robinson Batista <manuel7@outlook.es>
2024-12-21 02:15:09 -0500

    Add initial dataset to be loaded into the database

---
+++ source/Services/Catalog/Infrastructure/SeedData/CatalogTypeSeedData.cs
@@ -0,0 +1,26 @@
+﻿using EShopOnContainers.Catalog.Domain;
+
+namespace EShopOnContainers.Catalog.Infrastructure.SeedData;
+
+public static class CatalogTypeSeedData
+{
+    public static async Task Initialize(CatalogContext context)
+    {
+        if (context.CatalogTypes.Any())
+        {
+            return;
+        }
+
+        List<CatalogType> catalogTypes = new List<CatalogType>()
+        {
+            new CatalogType() { Type = "Mug"},
+            new CatalogType() { Type = "T-Shirt" },
+            new CatalogType() { Type = "Sheet" },
+            new CatalogType() { Type = "USB Memory Stick" }
+        };
+
+        context.CatalogTypes.AddRange(catalogTypes);
+        await context.SaveChangesAsync();
+    }
+}
+
