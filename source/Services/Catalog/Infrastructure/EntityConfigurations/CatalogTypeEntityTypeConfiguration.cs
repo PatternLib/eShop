7ba3cc307dd897108150f7efc01fab4b2529aca7
Robinson Batista <manuel7@outlook.es>
2024-12-21 02:12:39 -0500

    Configure entities for database mapping with Entity Framework Core

---
+++ source/Services/Catalog/Infrastructure/EntityConfigurations/CatalogTypeEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EShopOnContainers.Catalog.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EShopOnContainers.Catalog.Infrastructure.EntityConfigurations;
+
+public class CatalogTypeEntityTypeConfiguration
+    : IEntityTypeConfiguration<CatalogType>
+{
+    public void Configure(EntityTypeBuilder<CatalogType> builder)
+    {
+        builder.ToTable(name: "CatalogType");
+
+        builder.HasKey(keyExpression: x => x.Id);
+
+        builder.Property(propertyExpression: x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(propertyExpression: x => x.Type)
+            .IsRequired(required: true)
+            .HasMaxLength(maxLength: 100);
+    }
+}
