779f8dd65e310b791e0ef12e0cd6638857c4d7e6
Robinson Batista <manuel7@outlook.es>
2024-12-21 04:21:24 -0500

    Created the CatalogIndexViewModel to pass product data from the controller to the view

---
+++ source/Web/WebMvc/Models/CatalogIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EShopOnContainers.WebMvc.Domain;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EShopOnContainers.WebMvc.Models;
+
+public class CatalogIndexViewModel
+{
+    public IEnumerable<CatalogItem> CatalogItems { get; set; }
+    public IEnumerable<SelectListItem> Brands { get; set; }
+    public IEnumerable<SelectListItem> Types { get; set; }
+    public int? BrandFilterApplied { get; set; }
+    public int? TypeFilterApplied { get; set; }
+    public PaginationInfo PaginationInfo { get; set; }
+}
