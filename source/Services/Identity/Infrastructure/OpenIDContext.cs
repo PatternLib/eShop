5fb000c88fc9010fcd9579ea9529199711585074
Robinson Batista <manuel7@outlook.es>
2024-12-25 19:38:43 -0500

    Implemented a custom DbContext class that inherits from IdentityDbContext

---
+++ source/Services/Identity/Infrastructure/OpenIDContext.cs
@@ -0,0 +1,17 @@
+﻿using EShopOnContainers.Identity.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShopOnContainers.Identity.Infrastructure;
+
+public class OpenIDContext : IdentityDbContext<ApplicationUser>
+{
+    public OpenIDContext(DbContextOptions<OpenIDContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.UseOpenIddict();
+    }
+}
