f693c3d9ce4d96c03a1c38558251f06175fb657d
Robinson Batista <manuel7@outlook.es>
2024-12-26 03:34:51 -0500

    Simplify Program class by moving configuration to Startup class

--- source/Services/Identity/Program.cs
+++ source/Services/Identity/Program.cs
@@ -1,14 +1,30 @@
+using EShopOnContainers.Identity.Extensions;
+using EShopOnContainers.Identity.Infrastructure;
+using EShopOnContainers.Identity.Infrastructure.SeedData;
+using Microsoft.AspNetCore;
+
 namespace EShopOnContainers.Identity;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        BuildWebHost(args: args)
+            .MigrateDbContext<OpenIDContext>(seeder: (context, manager, config) =>
+            {
+                IdentitySeedData.Initialize(context).Wait();
+                OpenIDSeedData.Initialize(manager, config).Wait();
+            })
+            .Run();
+    }
 
-        app.MapGet("/", () => "Hello World!");
-
-        app.Run();
+    public static IWebHost BuildWebHost(string[] args)
+    {
+        return WebHost.CreateDefaultBuilder(args)
+            .UseKestrel()
+            .UseContentRoot(contentRoot: Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build();
     }
 }
