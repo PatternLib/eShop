b8fdb7ef3bf13928124d9d2dacb144d79a95481c
Robinson Batista <manuel7@outlook.es>
2024-12-26 04:05:20 -0500

    Add AccountController to handle redirect to OIDC provider

---
+++ source/Web/WebMvc/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EShopOnContainers.WebMvc.Controllers;
+
+[Authorize]
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<IActionResult> Login(string returnUrl)
+    {
+        var token = await HttpContext.GetTokenAsync(tokenName: "access_token");
+
+        if (token != null)
+        {
+            ViewData["access_token"] = token;
+        }
+
+        return RedirectToAction(actionName: "Index", controllerName: "Catalog");
+    }
+
+    public IActionResult Logout()
+    {
+        return SignOut(
+            authenticationSchemes: 
+            [
+                // Clear auth cookie
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                // Redirect to OIDC provider signout endpoint
+                OpenIdConnectDefaults.AuthenticationScheme
+            ],
+            properties: new AuthenticationProperties
+            {
+                RedirectUri = "/Account/Logout"
+            });
+    }
+}
