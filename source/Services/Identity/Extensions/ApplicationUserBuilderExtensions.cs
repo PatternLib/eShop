51b75fe8eccb0969be72b42d42ee4498c23ca294
Robinson Batista <manuel7@outlook.es>
2024-12-26 03:13:30 -0500

    Add ApplicationUserBuilderExtensions to decouple user creation for controller

---
+++ source/Services/Identity/Extensions/ApplicationUserBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using EShopOnContainers.Identity.Domain;
+using EShopOnContainers.Identity.Models;
+
+namespace EShopOnContainers.Identity.Extensions;
+
+public static class ApplicationUserBuilderExtensions
+{
+    public static ApplicationUser CreateUserExtension(this AccountRegisterViewModel.InputAccountRegister Input)
+    {
+        return new ApplicationUser()
+        {
+            CardHolderName = Input.User.CardHolderName,
+            CardNumber = Input.User.CardNumber,
+            CardType = Input.User.CardType,
+            SecurityNumber = Input.User.SecurityNumber,
+            Expiration = Input.User.Expiration,
+
+            Id = Guid.NewGuid().ToString(),
+            LastName = Input.User.LastName,
+            Name = Input.User.Name,
+            UserName = Input.Email,
+            Email = Input.Email,
+            PhoneNumber = Input.User.PhoneNumber,
+            ZipCode = Input.User.ZipCode,
+            Country = Input.User.Country,
+            State = Input.User.State,
+            City = Input.User.City,
+            Street = Input.User.Street,
+
+            NormalizedEmail = Input.Email.ToLower().ToUpper(),
+            NormalizedUserName = Input.Email.ToLower().ToUpper(),
+            SecurityStamp = Guid.NewGuid().ToString("D"),
+        };
+    }
+}
