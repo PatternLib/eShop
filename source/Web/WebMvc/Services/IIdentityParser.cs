5c54f7f9fc706897ee4d39fea88ec553e0399960
Robinson Batista <manuel7@outlook.es>
2024-12-30 16:41:29 -0500

    Add IIdentityParse services to map user claims to a strongly-typed

---
+++ source/Web/WebMvc/Services/IIdentityParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Principal;
+
+namespace EShopOnContainers.WebMvc.Services;
+
+/// <summary>
+/// Proporciona una abstracción para extraer la informacion del usuario autenticado en la aplicación
+/// </summary>
+public interface IIdentityParser<T>
+{
+    /// <summary>
+    /// Extrae la informacion del usuario del <see cref="IPrincipal"/> y la convierte en <see cref="T"/>.
+    /// </summary>
+    /// <param name="principal">La identidad del usuario autenticado en la aplicación.</param>
+    /// <returns>Un objeto de tipo <see cref="T"/> que representa al usuario autenticado.</returns>
+    T Parse(IPrincipal principal);
+}
