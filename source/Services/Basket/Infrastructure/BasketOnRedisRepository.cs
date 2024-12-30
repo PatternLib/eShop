75ca8f9ccc9decf964670f64990c2611b2e8270d
Robinson Batista <manuel7@outlook.es>
2024-12-30 15:34:39 -0500

    Implement repository for managing customer data on Redis

---
+++ source/Services/Basket/Infrastructure/BasketOnRedisRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using EShopOnContainers.Basket.Domain;
+using StackExchange.Redis;
+
+namespace EShopOnContainers.Basket.Infrastructure;
+
+public class BasketOnRedisRepository : IBasketRepository
+{
+    private readonly ILogger<BasketOnRedisRepository> _logger;
+    private readonly ConnectionMultiplexer _redis;
+    private readonly IDatabase _database;
+
+    public BasketOnRedisRepository(ILogger<BasketOnRedisRepository> logger, ConnectionMultiplexer redis)
+    {
+        _logger = logger;
+        _redis = redis;
+        _database = _redis.GetDatabase();
+    }
+
+    public async Task<CustomerBasket> GetBasketAsync(string customerId)
+    {
+        var data = await _database.StringGetAsync(key: customerId);
+
+        if (data.IsNullOrEmpty)
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<CustomerBasket>(json: data)!;
+    }
+
+    public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+    {
+        var created = await _database.StringSetAsync(
+            key: basket.BuyerId,
+            value: JsonSerializer.Serialize(value: basket));
+
+        if (!created)
+        {
+            _logger.LogInformation(message: "Problem occur persisting the item.");
+
+            return null;
+        }
+
+        _logger.LogInformation(message: "Basket item persisted succesfully.");
+
+        return await GetBasketAsync(customerId: basket.BuyerId);
+    }
+}
